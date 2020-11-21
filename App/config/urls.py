67b57123e3fb8f3eeb4cc9e5b6618491f26afef4
Carlos_Osuna <carlosalvaroosuna1@gmail.com>
2020-11-21 15:46:54 -0400

    Se agregó los modelos, y los index

--- App/config/urls.py
+++ App/config/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from config.views import *
+from django.views.generic import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('simon/',TemplateView.as_view(template_name='index.html')),
+    path('alfredo/',TemplateView.as_view(template_name='HtmlCB.html')),
+    path('jose/',TemplateView.as_view(template_name='index-jose.html'))
 ]
--- App/core/Licores/admin.py
+++ App/core/Licores/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from core.Licores.models import * 
 # Register your models here.
+
+admin.site.register(Licor)
+admin.site.register(Marca)
+admin.site.register(Material)
+admin.site.register(Tipo)
+admin.site.register(Tamano)
+admin.site.register(Sabor)
+admin.site.register(Pais)
+admin.site.register(Grado_Alcoholico)
